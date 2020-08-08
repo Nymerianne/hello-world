650ba4866b56e2d7f628f33fddefb4d35c71afe0
kida <murariu.alexandra2002@gmail.com>
2020-08-08 23:16:37 +0300

    upgraded version with symbolic constants

--- Exercise1-5.c
+++ Exercise1-5.c
@@ -1,20 +1,19 @@
  #include <stdio.h>
 
+#define LOWER 0    /* lower limit of temperature table */
+#define UPPER 300  /* upper limit */
+#define STEP 20    /* step size */
+
  /* print Fahrenheit-Celsius table --> FOR VERSION
      for fahr = 300, 280, ..., 0; floating-point with heading reverse version */
 
 main()
 {
   float fahr, celsius;
-  int lower, upper, step;
-
-  lower = 0;   /* lower limit of temperature table */
-  upper = 300; /* upper limit */
-  step = 20;   /* step size */
 
   printf("Fahrenheit-Celsius table --> floating-point version\n");
 
-  for (fahr = upper; fahr >= lower; fahr -= step) {
+  for (fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
     celsius = (5.0 / 9.0) * (fahr - 32.0);
     printf("%3.0f %6.1f\n", fahr, celsius);
   }
