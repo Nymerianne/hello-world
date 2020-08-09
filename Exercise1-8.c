2b25ce7395c6da616884a637f21198e6337b8b4c
kida <murariu.alexandra2002@gmail.com>
2020-08-09 13:30:05 +0300

    count the number of blanks, tabs and newlines in input

---
+++ Exercise1-8.c
@@ -0,0 +1,28 @@
+ #include<stdio.h>
+
+ /* count blanks, tabs and newlines in input */
+
+ int main()
+ {
+     int c, b, t, nl;
+
+     b = 0;
+     t = 0;     /* initialization of number of tabs, blanks and newlines to 0 */
+     nl = 0;
+
+     while ((c = getchar()) != EOF) {
+         if (c == ' ') {
+             ++b;
+         }
+         if (c == '\t') {
+             ++t;
+         }
+         if (c == '\n') {
+             ++nl;
+         }
+    }
+
+    printf("The input has %d blanks, %d tabs and %d newlines\n", b, t, nl);
+
+    return 0;
+ }
