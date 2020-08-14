fa329ca055f85fa6ed7fcfca5873d34b9997275b
kida <murariu.alexandra2002@gmail.com>
2020-08-14 21:27:11 +0300

    added print of total chars in arbitrarily long input

--- Exercise1-16.c
+++ Exercise1-16.c
@@ -4,20 +4,23 @@
 
 int main()
 {
-    int len, c, nr;
+    int len, c, nr, totalchars;
 
     len = 1; // initialize length and number of chars for first row
     nr = 1;
+    totalchars = 1; // initialize total number of chars of input text
+
     c = getchar();
 
     while ((c = getchar()) != EOF) {
         len++;
+        totalchars++;
         if(c == '\n') {
-            printf("line no %d is %d characters long\n", nr, len);
+            printf ("line no %d is %d characters long\n", nr, len);
             len = 0;
             nr++;
         }
     }
-
+    printf ("The input text has %d characters in total", totalchars);
     return 0;
 }
