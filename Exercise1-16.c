2c89c15571c6cb1f7ffffe7786b2b33ba648e409
kida <murariu.alexandra2002@gmail.com>
2020-08-14 22:38:27 +0300

    also print the max part of the longest line

--- Exercise1-16.c
+++ Exercise1-16.c
@@ -1,28 +1,66 @@
 #include <stdio.h>
 
-/* print the length of arbitrarily long input lines, and as much as possible of the text */
+#define MAXLINE 20  /* maximum input line size */
 
-int main()
-{
-    int len, c, nr, totalchars;
+/* print the length of arbitrarily long input lines and the text */
 
-    len = 1; // initialize length and number of chars for first row
-    nr = 1;
-    totalchars = 1; // initialize total number of chars of input text
+int getlines(char line[], int maxline);
+void copy(char to[], char from[]);
 
-    c = getchar();
+int main() {
 
-    while ((c = getchar()) != EOF) {
-        len++;
-        totalchars++;
-        if(c == '\n') {
-            printf ("line no %d is %d characters long\n", nr, len);
-            len = 0;
-            nr++;
+    int len; // current line length
+    int max; // max length seen so far
+    char line[MAXLINE]; // current input line
+    char longest[MAXLINE]; // longest line max part saved here
+
+    max = 0;
+
+    while ((len = getlines(line, MAXLINE)) > 0) {
+        if (len > max) {
+            max = len;
+            copy (longest, line);
         }
     }
 
-    printf ("The input text has %d characters in total", totalchars);
-    
+    if (max > 0) {
+        printf ("The longest line has the initial part: %s\n", longest);
+        printf ("The longest line has %d characters\n", max);
+    }
     return 0;
+}
+
+/* getline: read a line into s, return length */
+int getlines(char s[], int lim) {
+
+    int c;
+    int i;
+
+    for (i = 0; i < lim - 1 && ((c = getchar()) != EOF) && c != '\n'; ++i) {
+        s[i] = c;
+    }
+
+    if (i == lim - 1) {
+        while (((c = getchar()) != EOF) && c != '\n') {
+            ++i;
+        }
+        s[lim - 1] = '\0';
+    } else if (c == '\n') {
+        s[i] = c;
+        ++i;
+        s[i] = 0;
+    }
+    return i;
+}
+
+/* copy: copy 'from' into 'to' */
+void copy (char to[], char from[]) {
+
+    int i;
+
+    i = 0;
+
+    while ((to[i] = from[i]) != '\0') {
+        ++i;
+    }
 }
